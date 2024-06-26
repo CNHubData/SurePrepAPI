ec9d41a6afe5743165e85839bd85ae71aca51f59
CNHubData <139801556+CNHubData@users.noreply.github.com>
2024-06-26 08:26:12 -0600

    Shows Created Binder ID Output

--- CreateBinder.py
+++ CreateBinder.py
@@ -53,12 +53,10 @@ def create_binder():
     uri = "https://api.sureprep.com/V5.0/Binder/CreateBinder"
 
     body = {
-    "unique_identifier": "fo8f4d60-d0de-4668-8ed5-6bd4a4bd534b",
-    "Client_Id": "20802600000",
-    "First_Name": "Hans",
-    "Last_Name": "Haroldsen",
-    "SpouseFirstName": "Jordyn",
-    "SpouseLastName": "Haroldsen",
+    "unique_identifier": "12345",
+    "Client_Id": "123",
+    "First_Name": "Client",
+    "Last_Name": "Test",
     "email": "jarp@coopernorman.com",
     "service_Type_ID": 4,
     "template_ID": 27801610,
@@ -73,16 +71,8 @@ def create_binder():
     response = requests.post(uri, headers=headers, json=body)
 
     if response.status_code == 200:
-        response_data = response.json()
-        binder_ids = response_data.get("binder_ids")  # Adjust this to the actual key for binder IDs in the response
-        client_id = body["Client_Id"]
-        
-        if binder_ids:
-            binder_mapping = {client_id: binder_ids}
-            print(f"Binder created successfully. Binder mapping: {binder_mapping}")
-            return binder_mapping
-        else:
-            print("Binder created, but no Binder IDs found in response.")
+        print("Binder created successfully.")
+        print(response.json())
     else:
         print(f"Failed to create binder. Status code: {response.status_code}")
         print(response.text)
